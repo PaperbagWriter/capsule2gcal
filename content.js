e07aad5642946b44d39e5c606d3b8c0132e856e0
Jeremie Bastien <joetbandit@hotmail.com>
2016-01-11 20:07:22 -0500

    JS injection back and forth getDOM working

--- content.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(
-        function(request, sender, sendResponse){
-            if (request.action == "getDOM")
-                sendResponse({dom: $("body")});
-              else
-                sendResponse({});
-
-        });
